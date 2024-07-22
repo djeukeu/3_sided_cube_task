8fc4cd36a25b93733f03d02163cb50d884b7377d
djeukeu <dmxchristian@gmail.com>
2024-07-23 00:45:53 +0100

    feature test using PHPUnit

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_register_post_request(): void
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+}
